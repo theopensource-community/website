a990d4bc99b27eda2eac18c17981086cfdad0e89
xenophenes <xenophenes42@gmail.com>
2024-09-15 09:51:34 -0700

    Add Powered by Timescale @ the bottom

--- src/navigation.ts
+++ src/navigation.ts
@@ -86,7 +86,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://www.timescale.com/icon.ico" alt="Timescale logo" loading="lazy"></img>
+    Powered by <a class="text-blue-600 underline dark:text-muted" href="https://www.timescale.com">Timescale</a> · All rights reserved.
   `,
 };
