67bb6e6f52ce9c78dea2195b734e43cc44a0af08
xenophenes <xenophenes42@gmail.com>
2024-09-14 21:55:38 -0700

    Update init blog template & add Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: vercel(),
 });
